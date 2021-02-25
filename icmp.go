fcd5520d7d4ffbf839b51e52af0f7cdac7721b03
Aneurin Barker Snook <aneurin@edge.network>
2021-02-25 20:32:25 +0000

    fix: inconsistent self name "p" -> "d"

--- icmp.go
+++ icmp.go
@@ -74,48 +74,48 @@ func newProtocolHandler(addr *net.IPAddr) (h icmpProtocolHandler) {
 	return
 }
 
-func (p *icmpDriver) Address() net.Addr {
-	return p.config.Addr
+func (d *icmpDriver) Address() net.Addr {
+	return d.config.Addr
 }
 
-func (p *icmpDriver) Connect(ctx context.Context) error {
-	c, err := p.protocolHandler.Listen("")
+func (d *icmpDriver) Connect(ctx context.Context) error {
+	c, err := d.protocolHandler.Listen("")
 	if err != nil {
 		return err
 	}
 
-	p.ctx, p.cancel = context.WithCancel(ctx)
-	p.messageProvider = newICMPMessageProvider(p.protocolHandler, p.config.Addr)
-	p.packetConn = c
-	p.chanRawPacket = make(chan RawPacket)
+	d.ctx, d.cancel = context.WithCancel(ctx)
+	d.messageProvider = newICMPMessageProvider(d.protocolHandler, d.config.Addr)
+	d.packetConn = c
+	d.chanRawPacket = make(chan RawPacket)
 
-	go p.recv()
+	go d.recv()
 	return nil
 }
 
-func (p *icmpDriver) Disconnect() error {
-	p.cancel()
-	err := p.packetConn.Close()
+func (d *icmpDriver) Disconnect() error {
+	d.cancel()
+	err := d.packetConn.Close()
 	return err
 }
 
-func (p *icmpDriver) Ping(timer *Timer) (RawPacket, error) {
+func (d *icmpDriver) Ping(timer *Timer) (RawPacket, error) {
 	errc := make(chan error, 1)
 	defer close(errc)
 	go func() {
-		if err := p.send(timer); err != nil {
+		if err := d.send(timer); err != nil {
 			errc <- err
 		}
 	}()
 
 	for {
 		select {
-		case <-p.ctx.Done():
-			return RawPacket{}, p.ctx.Err()
+		case <-d.ctx.Done():
+			return RawPacket{}, d.ctx.Err()
 		case err := <-errc:
 			return RawPacket{}, err
-		case raw := <-p.chanRawPacket:
-			if err := p.handlePacket(raw); err != nil {
+		case raw := <-d.chanRawPacket:
+			if err := d.handlePacket(raw); err != nil {
 				if isICMPIgnoredPacket(err) {
 					continue
 				}
@@ -126,27 +126,27 @@ func (p *icmpDriver) Ping(timer *Timer) (RawPacket, error) {
 	}
 }
 
-func (p *icmpDriver) handlePacket(raw RawPacket) error {
-	msg, err := p.protocolHandler.Parse(raw.Message)
+func (d *icmpDriver) handlePacket(raw RawPacket) error {
+	msg, err := d.protocolHandler.Parse(raw.Message)
 	if err != nil {
 		return err
 	}
 	echo := msg.Body.(*icmp.Echo)
 	// ignore if not echo reply
-	if msg.Type != p.protocolHandler.ReplyType() {
+	if msg.Type != d.protocolHandler.ReplyType() {
 		return ErrICMPIgnoredPacket
 	}
 	// ignore if id mismatched
-	if echo.ID != p.messageProvider.id {
+	if echo.ID != d.messageProvider.id {
 		return ErrICMPIgnoredPacket
 	}
 
-	track, _, err := p.messageProvider.ReadData(msg)
+	track, _, err := d.messageProvider.ReadData(msg)
 	if err != nil {
 		return err
 	}
 	// ignore if tracker mismatched
-	if track != p.messageProvider.tracker {
+	if track != d.messageProvider.tracker {
 		return ErrICMPIgnoredPacket
 	}
 
@@ -154,14 +154,14 @@ func (p *icmpDriver) handlePacket(raw RawPacket) error {
 	return nil
 }
 
-func (p *icmpDriver) recv() error {
-	defer close(p.chanRawPacket)
+func (d *icmpDriver) recv() error {
+	defer close(d.chanRawPacket)
 	for {
 		select {
-		case <-p.ctx.Done():
-			return p.ctx.Err()
+		case <-d.ctx.Done():
+			return d.ctx.Err()
 		default:
-			raw, err := p.recvPacket()
+			raw, err := d.recvPacket()
 			if netErr, ok := err.(*net.OpError); ok {
 				if netErr.Timeout() {
 					continue
@@ -169,16 +169,16 @@ func (p *icmpDriver) recv() error {
 					return err
 				}
 			}
-			p.chanRawPacket <- raw
+			d.chanRawPacket <- raw
 		}
 	}
 }
 
-func (p *icmpDriver) recvPacket() (RawPacket, error) {
-	if err := p.packetConn.SetReadDeadline(time.Now().Add(p.config.ReadTimeout)); err != nil {
+func (d *icmpDriver) recvPacket() (RawPacket, error) {
+	if err := d.packetConn.SetReadDeadline(time.Now().Add(d.config.ReadTimeout)); err != nil {
 		return RawPacket{}, err
 	}
-	b, nb, ttl, err := p.protocolHandler.Read(p.packetConn)
+	b, nb, ttl, err := d.protocolHandler.Read(d.packetConn)
 	if err != nil {
 		return RawPacket{}, err
 	}
@@ -190,8 +190,8 @@ func (p *icmpDriver) recvPacket() (RawPacket, error) {
 	return raw, nil
 }
 
-func (p *icmpDriver) send(timer *Timer) error {
-	msg := p.messageProvider.Provide()
+func (d *icmpDriver) send(timer *Timer) error {
+	msg := d.messageProvider.Provide()
 	msgBytes, err := msg.Marshal(nil)
 	if err != nil {
 		return err
@@ -199,7 +199,7 @@ func (p *icmpDriver) send(timer *Timer) error {
 
 	timer.Start()
 	for {
-		_, err := p.packetConn.WriteTo(msgBytes, p.config.Addr)
+		_, err := d.packetConn.WriteTo(msgBytes, d.config.Addr)
 		if err != nil {
 			netErr, ok := err.(*net.OpError)
 			if ok && netErr.Err == syscall.ENOBUFS {
